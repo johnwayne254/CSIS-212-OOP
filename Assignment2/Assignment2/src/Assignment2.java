4ebab9214b01d0e791a228aeef8f64fc3cf60d5d
johnwayne254 <62761419+johnwayne254@users.noreply.github.com>
2024-09-09 20:11:41 +0300

    Assignment2.java

--- Assignment2/Assignment2/src/Assignment2.java
+++ Assignment2/Assignment2/src/Assignment2.java
@@ -1,14 +1,13 @@
-//Assignment2 -- This is assignment two
 import java.util.Scanner; // program imports the scanner to input employee data
 
 public class Assignment2 {
 
 	public static void main(String[] args) {
-		System.out.println("Stanley Soman - Assignment 2\n"); // Heading
+		System.out.println("Amarachi Mazariegos- Assignment 2\n"); // Heading
 		
 		//Exercise 3.20 JHTP (Salary Calculator)
 		
-		// create scanner to obtain salary information
+		// Scanner to obtain salary information
 		Scanner input = new Scanner(System.in);
 
 		//gross pay init
@@ -21,12 +20,11 @@ public class Assignment2 {
 		while (employeecounter <= 3) // For three employees loop 3 times
 		{ 
 			System.out.print("Enter the hourly rate: "); // prompt for employee rate
-			double hourly = input.nextDouble(); // We want to use double to get the exact value
+			double hourly = input.nextDouble(); 
 			
 			System.out.print("Enter hours worked "); // prompt for hours worked
-			double totalhrs = input.nextDouble(); // hours should be double to prevent compilation errors
+			double totalhrs = input.nextDouble(); 
 			
-			//overtime code
 			if (totalhrs > 40) {
 				double over40 = totalhrs - 40; //if its over 40 hours, we just subtract 40 from that to find out how much over we worked
 				totalpay = ( 40 * hourly ) + (over40 * hourly * 1.5); // get normal pay + our overtime hrs times time and half
@@ -36,8 +34,8 @@ public class Assignment2 {
 			//Alternate way
 			//System.out.println("Pay for employee " + employeecounter + " is " + "$" + totalpay); // Use ln for natural line skipping
 			employeecounter = employeecounter + 1; //onto the next employee
-		} // end our while loop
+		} 
 		
-	} //end main
+	} 
 
-} // end assignment 2
+}
