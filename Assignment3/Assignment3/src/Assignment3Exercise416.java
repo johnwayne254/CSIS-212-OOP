c9c90b71204b8224317f3495b1d4b6f8c2e47272
johnwayne254 <62761419+johnwayne254@users.noreply.github.com>
2024-09-09 22:18:39 +0300

    Assignment Exercise 416.java

--- Assignment3/Assignment3/src/Assignment3Exercise416.java
+++ Assignment3/Assignment3/src/Assignment3Exercise416.java
@@ -1,28 +1,34 @@
 import java.util.Scanner;
 
-public class BarChart {
+public class Assignment3Exercise416 {
     public static void main(String[] args) {
-        System.out.println("Amarachi Mazariegos - Assignment 3 Exercise 4.16 Bar Chart Printing Program \n"); // Heading
+        System.out.println("Amarachi Mazariegos - Assignment 3 Exercise 4.16 Bar Chart Printing Program \n");
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         int[] numbers = new int[5];
+        int fivenumbers = 0;
+
+        System.out.println("Enter five numbers between 1-30:");
 
         // Read 5 numbers between 1 and 30
-        for (int i = 0; i < 5; i++) {
-            do {
-                System.out.print("Enter number " + (i + 1) + " (between 1 and 30): ");
-                numbers[i] = scanner.nextInt();
-            } while (numbers[i] < 1 || numbers[i] > 30);
+        while (fivenumbers < 5) {
+            int inputinteger = input.nextInt();
+            if (inputinteger >= 1 && inputinteger <= 30) {
+                numbers[fivenumbers] = inputinteger;
+                fivenumbers++;
+            } else {
+                System.out.println("You need to pick a number between 1 and 30.");
+            }
         }
 
         // Display bar chart
-        for (int number : numbers) {
-            for (int j = 0; j < number; j++) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < numbers[i]; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
 
-        scanner.close();
+        input.close();
     }
 }
